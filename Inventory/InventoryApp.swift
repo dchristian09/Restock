abb5bcbd74d5afbc0f5dd0d6d663c679610c3f79
dchristian09 <dchristian09@student.ciputra.ac.id>
2023-06-13 14:24:41 +0700

    Initial Commit

---
+++ Inventory/InventoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  InventoryApp.swift
+//  Inventory
+//
+//  Created by David Christian on 13/06/23.
+//
+
+import SwiftUI
+
+@main
+struct InventoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
